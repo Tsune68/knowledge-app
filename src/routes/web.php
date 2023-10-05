fee8a2304731928b4d9577e3e677ecee6b038666
恒川泰智 <tsunekawataichi@Hajimari-TsunekawaTaichi.local>
2023-10-05 16:42:55 +0900

    web.phpを修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/slack/redirect', [App\Http\Controllers\OAuthController::class, 'redirect']);
     Route::get('/slack/callback', [App\Http\Controllers\OAuthController::class, 'callback']);
-    Route::get('/login', [App\Http\Controllers\PostController::class, 'showLogin'])->name('login');
+    Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLogin'])->name('login');
 });
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [App\Http\Controllers\PostController::class, 'showTopPage'])->name('index');
-    Route::get('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
+    Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 });
